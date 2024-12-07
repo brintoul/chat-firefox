c083e7fd4d781be6f7589a1fd90c353883d6110b
Bradley Rintoul <brintoul@controlledthinking.com>
2024-12-07 10:43:02 -0800

    Update content-script.js to remove logging

--- content-script.js
+++ content-script.js
@@ -1,5 +1,4 @@
 document.addEventListener('keydown', (event) => {
-  console.log("KEYDOWN!!");
   if (event.key === 'Enter') {
       const sendButton = document.querySelector('button[aria-label="Send prompt"]');
       if (sendButton) {
